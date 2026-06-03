845516532ae9a70f9c55edf91241fe5e45e0f132
Roy Weesner <rweesner@smatechnologies.com>
2026-06-03 08:33:56 -0500

    First Commit

---
+++ docusaurus.config.js
@@ -0,0 +1,53 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Continuous Help',
+  tagline: 'ACS Fiserv CCM Connector',
+  url: 'https://help.smatechnologies.com',
+  baseUrl: '/opcon/connectors/acs-fiservccm/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'smatechnologies',
+  projectName: 'acs-fiservccm-docs',
+  themeConfig: {
+    navbar: {
+      title: 'Help',
+      logo: {
+        alt: 'SMA Technologies Help Logo',
+        src: 'img/logo.svg',
+        href: 'https://help.smatechnologies.com',
+      },
+    },
+    footer: {
+      style: 'dark',
+      copyright: `Copyright © ${new Date().getFullYear()} SMA Technologies.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          breadcrumbs: false,
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          editUrl:
+            'https://github.com/smatechnologies/acs-fiservccm-docs/blob/main',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+        gtag: {
+          trackingID: 'G-7XYMFXX81Y',
+        },
+      },
+    ],
+  ],
+  plugins: [
+//    [
+//      require.resolve('@cmfcmf/docusaurus-search-local'), 
+//      {
+//      }
+//    ],
+  ],
+};
